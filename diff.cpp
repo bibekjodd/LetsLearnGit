a5895dfa43f54f1ae7045fb90d39767ea998444c
Bibek Bhattarai <bibekjodd@outlook.com>
2022-09-05 08:13:12 +0545

    added diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int diff(int x, int y)
+{
+    return x - y;
+}
+
+int main()
+{
+    int x, y;
+    cout << diff(x, y) << "\n";
+
+    return 0;
+}
